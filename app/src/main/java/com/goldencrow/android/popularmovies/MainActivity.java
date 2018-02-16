239269d85365dc20fb18259504c789faff5e7949
Philipp <philipp.hermueller@gmx.at>
2018-02-17 00:41:23 +0100

    Connect to the API The movies are now retrieved from the moviesDB-API.

--- app/src/main/java/com/goldencrow/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/goldencrow/android/popularmovies/MainActivity.java
@@ -1,13 +1,105 @@
+
+/*
+ * Author: Philipp Hermüller
+ */
+
 package com.goldencrow.android.popularmovies;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.goldencrow.android.popularmovies.entities.Movie;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String API_SCHEME = "http";
+    private static final String API_AUTHORITY = "api.themoviedb.org";
+    private static final String API_VERSION_PATH = "3";
+    private static final String API_MOVIES_PATH = "movie";
+    private static final String POPULAR_API_PATH = "popular";
+    private static final String TOP_RATED_API_PATH = "top_rated";
+    private static final String API_KEY_NAME = "api_key";
+    private static final String API_KEY
+            = "";  // insert API-KEY from themoviedb.org HERE
+
+    private Movie[] mMovies;
+
+    @BindView(R.id.json_tf)
+    TextView mJsonTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        //This is for later usage in the DetailActivity:
+        //Locale current = getResources().getConfiguration().locale;
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String apiUri = getApiUri(POPULAR_API_PATH);
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                apiUri,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String jsonResponse) {
+                        Gson gson = new Gson();
+                        try {
+                            JSONObject result = new JSONObject(jsonResponse);
+                            String jsonArrayMovies = result.get("results").toString();
+
+                            mMovies = gson.fromJson(jsonArrayMovies, Movie[].class);
+                        } catch (JSONException e) {
+                            Toast.makeText(
+                                    MainActivity.this,
+                                    "Movie data could not be read!",
+                                    Toast.LENGTH_SHORT).show();
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(
+                                MainActivity.this,
+                                "There was a network Error!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    private String getApiUri(String apiPath) {
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme(API_SCHEME)
+                .authority(API_AUTHORITY)
+                .appendPath(API_VERSION_PATH)
+                .appendPath(API_MOVIES_PATH)
+                .appendPath(apiPath)
+                .appendQueryParameter(API_KEY_NAME, API_KEY);
+        return builder.build().toString();
     }
 }
--- app/src/main/java/com/goldencrow/android/popularmovies/entities/Movie.java
+++ app/src/main/java/com/goldencrow/android/popularmovies/entities/Movie.java
@@ -1,8 +1,87 @@
-package com.goldencrow.android.popularmovies.entities;
 
-/**
- * Created by Philipp on 16.02.18.
+/*
+ * Author: Philipp Hermüller
  */
 
+package com.goldencrow.android.popularmovies.entities;
+
+import android.annotation.SuppressLint;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Movie {
+    private double vote_average;
+    private String title;
+    private String poster_path;
+    private String overview;
+    private Date release_date;
+
+    public Movie() {
+    }
+
+    public Movie(double voteAverage, String title, String posterPath, String overview, Date releaseDate) {
+        this.vote_average = voteAverage;
+        this.title = title;
+        this.poster_path = posterPath;
+        this.overview = overview;
+        this.release_date = releaseDate;
+    }
+
+    public double getVoteAverage() {
+        return vote_average;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.vote_average = voteAverage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return poster_path;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.poster_path = posterPath;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Date getReleaseDate() {
+        return release_date;
+    }
+
+    public String getReleaseDateAsString(Locale locale) {
+        DateFormat df = SimpleDateFormat.getDateInstance(DateFormat.SHORT, locale);
+        return df.format(release_date);
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        // I'm suppressing Lint because the format below isn't system specific but the format
+        //     from the API-server. So I can't use the System Format here.
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            this.release_date = format.parse(releaseDate);
+        } catch (ParseException e) {
+            this.release_date = null;
+        }
+    }
 }
