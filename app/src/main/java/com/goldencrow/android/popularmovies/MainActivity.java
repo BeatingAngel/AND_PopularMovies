b44c9272ccb53b63d6e34af97b14e913fc8688ca
Philipp <philipp.hermueller@gmx.at>
2018-02-17 12:30:43 +0100

    Handling Error Messages Errors and Network Connection Errors are now handled. The movies can now also be sorted by popularity or ratings.

--- app/src/main/java/com/goldencrow/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/goldencrow/android/popularmovies/MainActivity.java
@@ -10,8 +10,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -42,10 +47,18 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String MOVIE_LIST_JSON_KEY = "results";
 
-    private Movie[] mMovies;
-
     @BindView(R.id.movie_list_rv)
     RecyclerView mMoviesListRv;
+    @BindView(R.id.loading_indicator_pb)
+    ProgressBar mLoadingIndicatorPb;
+    @BindView(R.id.error_message_tf)
+    TextView mErrorMessageTf;
+    @BindView(R.id.reload_btn)
+    Button mReloadBtn;
+
+    private MovieAdapter mAdapter;
+    private RequestQueue mQueue;
+    private String mLoadingPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,14 +67,94 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        initializeRecyclerView();
+        mReloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showLoading();
+                queueStringRequest(mLoadingPath);
+            }
+        });
+
         //This is for later usage in the DetailActivity:
         //Locale current = getResources().getConfiguration().locale;
 
         // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String apiUri = getApiUri(POPULAR_API_PATH);
+        mQueue = Volley.newRequestQueue(this);
 
-        StringRequest stringRequest = new StringRequest(
+        showLoading();
+
+        // show order by popularity as default.
+        mLoadingPath = POPULAR_API_PATH;
+        queueStringRequest(mLoadingPath);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.main_page, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_sort_order) {
+            toggleSortOrder(item);
+            showLoading();
+            queueStringRequest(mLoadingPath);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showErrorMessage(int message) {
+        if (mMoviesListRv.getVisibility() == View.VISIBLE) {
+            mMoviesListRv.setVisibility(View.INVISIBLE);
+        } else {
+            mLoadingIndicatorPb.setVisibility(View.INVISIBLE);
+        }
+
+        mReloadBtn.setVisibility(View.VISIBLE);
+        mErrorMessageTf.setVisibility(View.VISIBLE);
+        mErrorMessageTf.setText(message);
+    }
+
+    private void showLoading() {
+        mLoadingIndicatorPb.setVisibility(View.VISIBLE);
+        mMoviesListRv.setVisibility(View.INVISIBLE);
+        if (mErrorMessageTf.getVisibility() == View.VISIBLE) {
+            mErrorMessageTf.setVisibility(View.INVISIBLE);
+            mReloadBtn.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void showData() {
+        mLoadingIndicatorPb.setVisibility(View.INVISIBLE);
+        mMoviesListRv.setVisibility(View.VISIBLE);
+        if (mErrorMessageTf.getVisibility() == View.VISIBLE) {
+            mErrorMessageTf.setVisibility(View.INVISIBLE);
+            mReloadBtn.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void toggleSortOrder(MenuItem menuItem) {
+        if (mLoadingPath.equals(POPULAR_API_PATH)) {
+            mLoadingPath = TOP_RATED_API_PATH;
+            menuItem.setTitle(R.string.action_sort_popularity);
+        } else {
+            mLoadingPath = POPULAR_API_PATH;
+            menuItem.setTitle(R.string.action_sort_ratings);
+        }
+    }
+
+    private void queueStringRequest(String apiPath) {
+        String apiUri = getApiUri(apiPath);
+
+        StringRequest request = new StringRequest(
                 Request.Method.GET,
                 apiUri,
                 new Response.Listener<String>() {
@@ -72,14 +165,13 @@ public class MainActivity extends AppCompatActivity {
                             JSONObject result = new JSONObject(jsonResponse);
                             String jsonArrayMovies = result.get(MOVIE_LIST_JSON_KEY).toString();
 
-                            mMovies = gson.fromJson(jsonArrayMovies, Movie[].class);
+                            Movie[] movies = gson.fromJson(jsonArrayMovies, Movie[].class);
 
-                            initializeRecyclerView();
+                            mAdapter.setMovieData(movies);
+
+                            showData();
                         } catch (JSONException e) {
-                            Toast.makeText(
-                                    MainActivity.this,
-                                    "Movie data could not be read!",
-                                    Toast.LENGTH_SHORT).show();
+                            showErrorMessage(R.string.error_message_parsing_json);
                             e.printStackTrace();
                         }
                     }
@@ -87,27 +179,22 @@ public class MainActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(
-                                MainActivity.this,
-                                "There was a network Error!",
-                                Toast.LENGTH_SHORT).show();
+                        showErrorMessage(R.string.error_message_connecting_to_api);
                     }
                 });
 
         // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        mQueue.add(request);
     }
 
     private void initializeRecyclerView() {
         GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
 
-        MovieAdapter mAdapter = new MovieAdapter(this);
+        mAdapter = new MovieAdapter(this);
         mMoviesListRv.setAdapter(mAdapter);
 
         mMoviesListRv.setLayoutManager(layoutManager);
         mMoviesListRv.setHasFixedSize(true);
-
-        mAdapter.setMovieData(mMovies);
     }
 
     private String getApiUri(String apiPath) {
