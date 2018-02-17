8d49f096f724f1ecb20266909a5c6839eb048403
Philipp <philipp.hermueller@gmx.at>
2018-02-17 17:41:13 +0100

    removed unused import.

--- app/src/main/java/com/goldencrow/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/goldencrow/android/popularmovies/MainActivity.java
@@ -18,7 +18,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
