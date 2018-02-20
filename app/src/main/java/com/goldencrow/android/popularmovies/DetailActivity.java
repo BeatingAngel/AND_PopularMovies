b8f9d60c6d4b65c9bbf57b8e3805fca08f956ce9
Philipp <philipp.hermueller@gmx.at>
2018-02-20 01:16:47 +0100

    fixed bug

--- app/src/main/java/com/goldencrow/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/goldencrow/android/popularmovies/DetailActivity.java
@@ -163,7 +163,6 @@ public class DetailActivity extends AppCompatActivity {
         if (cursor != null) {
             if (cursor.getCount() > 0) {
                 mFavoriteFaBtn.setImageResource(android.R.drawable.btn_star_big_on);
-                menu.findItem(R.id.action_favorite).setIcon(getDrawable(android.R.drawable.btn_star_big_on));
                 isFavorite = true;
             }
             cursor.close();
